b877db8ecadb2e17da3386a1aadeca2dd90f8f0a
dyego <dyego.chavez@gmail.com>
2020-11-27 13:01:47 +1300

    Adding buildspec

--- src/App.js
+++ src/App.js
@@ -5,18 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Test get AWS Secret Manager variables</h2>
+        <h3>My secret password is: {process.env.REACT_APP_SECRET_PASSWORD}</h3>
       </header>
     </div>
   );
